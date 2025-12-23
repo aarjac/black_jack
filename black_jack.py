0277f3000ae38be1fd652f81dd0ca87b3ae9ba3b
aarjac <56480900+aarjac@users.noreply.github.com>
2025-12-23 17:05:36 -0500

    Build class methods and game logic

--- black_jack.py
+++ black_jack.py
@@ -26,7 +26,9 @@ class Deck():
         shuffle(self.all_cards)
 
     def deal_one(self):
-        pass
+        card_to_deal = self.all_cards[0]
+        self.all_cards.pop(0)
+        return card_to_deal
 
     def __str__(self):
         return str(self.all_cards)
@@ -38,21 +40,60 @@ class Card():
         self.suit = suit
         self.value = value
 
+    #remove value from return
     def __str__(self):
         return self.rank + ' of ' + self.suit + ': ' + str(self.value)
 
 class Dealer():
-    pass
+    
+    def __init__(self):
+        self.hand = []
+
+    def hit(self):
+        pass
 
 class Player():
 
-    def __init__(self, bank, hand):
-        self.bank = bank
+    bank_amount = 500
+
+    def __init__(self, bank_amount = 500):
         self.hand = []
+        self.bank_amount = bank_amount
+
+    def bet(self):
+
+        while True:
+            try:
+                bet_amount = int(input('How much would you like to bet? '))
+
+            except ValueError:
+                print('That is not a valid input...')
+                print('I will ask again.\n')
+                continue
+
+            if 0 < bet_amount <= self.bank_amount:
+                self.bank_amount -= bet_amount
+                print('You have bet: $' + str(bet_amount))
+                print('Your new balance is: $' + str(self.bank_amount) + '\n')
+                return bet_amount
+            elif bet_amount > self.bank_amount:
+                print('You do not have enough money to bet that amount.')
+                print('Try again...\n')
+            elif bet_amount <= 0:
+                print('You must bet a positive value.')
+                print('Try again...\n')
 
 def main():
 
+    game_on = True
     new_deck = Deck()
+    black_jack = 21
+    acceptable_choices =  ['Y', 'N']
+    player_choice = ''
+    the_dealer = Dealer()
+    dealer_total = 0
+    player = Player()
+    player_total = 0
     
     for rank in ranks:
         for suit in suits: 
@@ -61,11 +102,49 @@ def main():
             new_deck.all_cards.append(new_card)
 
     new_deck.build_deck()
+    new_deck.shuffle()
 
-    for card in new_deck.all_cards:
+
+    current_bet = player.bet()
+
+    for _ in range(2):
+       player.hand.append(new_deck.deal_one())
+    
+    print("Player's Cards: ")
+    for card in player.hand:
         print(card)
+        player_total += card.value
+    print('Total: ' + str(player_total))
+    print('\n')
 
-    print(type(new_deck))
+    for _ in range(2):
+        the_dealer.hand.append(new_deck.deal_one())
+
+    #only show one of dealer's cards
+    print("The Dealer's Cards: ")
+    for card in the_dealer.hand:
+        print(card)
+        dealer_total += card.value
+    print('\n')
+
+    while player_total < black_jack:
+        while player_choice not in acceptable_choices:
+            player_choice = input('Would you like to hit (Y or N)? ')
+            print('\n')
+            if player_choice == 'Y':
+                player.hand.append(new_deck.deal_one())
+                print("Player's Cards: ")
+                for card in player.hand:
+                    print(card)
+                    player_total += card.value
+                print('Total: ' + str(player_total))
+                print('\n')
+            elif player_choice == 'N':
+                break
+            else: 
+                print('That was not a valid answer. Try again...')
+            
+            
 
 if __name__ == '__main__':
     main()
