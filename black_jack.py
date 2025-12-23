b3dddec9676c239b0b220f01df6445545c292416
aarjac <56480900+aarjac@users.noreply.github.com>
2025-12-23 18:32:08 -0500

    Implement additional game logic

--- black_jack.py
+++ black_jack.py
@@ -1,11 +1,11 @@
 from random import shuffle
 
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-ranks = ['One', 'Two', 'Three',
+ranks = ['Two', 'Three',
         'Four', 'Five', 'Six', 
         'Seven', 'Eight', 'Nine', 'Ten', 
         'Jack', 'Queen', 'King', 'Ace']
-values = {'One': 1, 'Two': 2, 'Three': 3, 
+values = {'Two': 2, 'Three': 3, 
         'Four': 4, 'Five': 5, 'Six': 6, 
         'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 
         'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': [1, 11]}
@@ -83,6 +83,22 @@ class Player():
                 print('You must bet a positive value.')
                 print('Try again...\n')
 
+def check_bust(player_total, dealer_total):
+    if player_total > 21:
+        return 'Dealer'
+    elif dealer_total >21:
+        return 'Player'
+    else:
+        return ''
+    
+def check_winner(player_total, dealer_total):
+    if player_total <= 21 and player_total > dealer_total:
+        return 'Player'
+    if dealer_total <= 21 and dealer_total > player_total:
+        return 'Dealer'
+    else: 
+        pass
+
 def main():
 
     game_on = True
@@ -94,7 +110,8 @@ def main():
     dealer_total = 0
     player = Player()
     player_total = 0
-    
+    winner = ''
+   
     for rank in ranks:
         for suit in suits: 
             new_card_value = (values[rank])
@@ -113,7 +130,13 @@ def main():
     print("Player's Cards: ")
     for card in player.hand:
         print(card)
-        player_total += card.value
+        if type(card.value) is int:
+            player_total += card.value
+        else:
+            if (player_total + card.value[1] > black_jack):
+                player_total += card.value[0]
+            else:
+                player_total += card.value[1]
     print('Total: ' + str(player_total))
     print('\n')
 
@@ -124,27 +147,82 @@ def main():
     print("The Dealer's Cards: ")
     for card in the_dealer.hand:
         print(card)
-        dealer_total += card.value
+        if type(card.value) is int:
+            dealer_total += card.value
+        else:
+            if (dealer_total + card.value[1] > black_jack):
+                dealer_total += card.value[0]
+            else:
+                dealer_total += card.value[1]
     print('\n')
 
     while player_total < black_jack:
         while player_choice not in acceptable_choices:
             player_choice = input('Would you like to hit (Y or N)? ')
             print('\n')
+            player_choice.upper()
             if player_choice == 'Y':
+                player_total = 0
                 player.hand.append(new_deck.deal_one())
                 print("Player's Cards: ")
                 for card in player.hand:
                     print(card)
-                    player_total += card.value
+                    if type(card.value) is int:
+                        player_total += card.value
+                    else:
+                        if (player_total + card.value[1] > black_jack):
+                            player_total += card.value[0]
+                        else:
+                            player_total += card.value[1]
                 print('Total: ' + str(player_total))
                 print('\n')
             elif player_choice == 'N':
                 break
             else: 
                 print('That was not a valid answer. Try again...')
-            
-            
+        if player_choice == 'N':
+            break
+        else:
+            pass
+        player_choice = ''
+    
+    winner = check_bust(player_total, dealer_total)
 
+    dealer_total = 0
+    print("The Dealer's Cards: ")
+    for card in the_dealer.hand:
+        print(card)
+        if type(card.value) is int:
+            dealer_total += card.value
+        else:
+            if (dealer_total + card.value[1] > black_jack):
+                dealer_total += card.value[0]
+            else:
+                dealer_total += card.value[1]
+    print('\n')    
+
+    while dealer_total <= player_total:
+        print('Dealer hits...')
+        the_dealer.hand.append(new_deck.deal_one())
+        dealer_total = 0
+        print("The Dealer's Cards: ")
+        for card in the_dealer.hand:
+            print(card)
+            if type(card.value) is int:
+                dealer_total += card.value
+            else:
+                if (dealer_total + card.value[1] > black_jack):
+                    dealer_total += card.value[0]
+                else:
+                    dealer_total += card.value[1]
+        print('Total: ' + str(dealer_total))
+        print('\n')
+        winner = check_bust(player_total, dealer_total)
+
+        if winner != '':
+            break
+    winner = check_winner(player_total, dealer_total)
+
+    print(winner + ' is the winner!')
 if __name__ == '__main__':
     main()
